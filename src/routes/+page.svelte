0a42d8404a47d27e4ddc88ae2d5de89630188dc7
acidmath <adrian.ajx@gmail.com>
2024-11-29 14:22:31 -0600

    add a basic input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import PrimeBoy from "../components/primeBoy.svelte";
+</script>
+
 <h1>Hello, I am a website created by Adrian Harrison</h1>
 <h3>He's a desktop developer, so let's not judge me too much</h3>
+<PrimeBoy/>
