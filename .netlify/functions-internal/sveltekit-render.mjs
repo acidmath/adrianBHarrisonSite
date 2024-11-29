fc0afaf70987fcca80499aabf976139df8da4f4f
acidmath <adrian.ajx@gmail.com>
2024-11-29 11:44:08 -0600

    add netlify adapter

---
+++ .netlify/functions-internal/sveltekit-render.mjs
@@ -0,0 +1,37 @@
+import { init } from '../serverless.js';
+
+export const handler = init((() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.BGdaovie.js","app":"_app/immutable/entry/app.SF_urcG5.js","imports":["_app/immutable/entry/start.BGdaovie.js","_app/immutable/chunks/entry.D1IdaYmH.js","_app/immutable/chunks/runtime.G6EyfWjc.js","_app/immutable/entry/app.SF_urcG5.js","_app/immutable/chunks/runtime.G6EyfWjc.js","_app/immutable/chunks/store.snWgdinY.js","_app/immutable/chunks/disclose-version.-Rdx7w0q.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		nodes: [
+			__memo(() => import('../server/nodes/0.js')),
+			__memo(() => import('../server/nodes/1.js')),
+			__memo(() => import('../server/nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})());
---
+++ .netlify/server/nodes/2.js
@@ -0,0 +1,8 @@
+
+
+export const index = 2;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/2.DmicTFuo.js","_app/immutable/chunks/disclose-version.-Rdx7w0q.js","_app/immutable/chunks/runtime.G6EyfWjc.js","_app/immutable/chunks/legacy.DviaO2oA.js"];
+export const stylesheets = [];
+export const fonts = [];
