f27e9f79670ceed9acdf11959d7f763d9cd612e8
acidmath <adrian.ajx@gmail.com>
2024-11-29 15:39:01 -0600

    a home brewed square root test

--- src/scripts/mathFunctions.ts
+++ src/scripts/mathFunctions.ts
@@ -1,3 +1,18 @@
+// I have the benefit of knowing I only need to output an integer
+// and it can be rounded down
+export function SquareRoot(input: number){
+    if(input <= 4) return 1;
+    let x: number = 2;
+    let xSquared = 4;
+    while(xSquared < input){
+        if(xSquared==input)return x;        
+        xSquared = ++x*x; // being fancy for fun
+    }
+    return x - 1;
+}
+
 export function IsPrime(input: number){
-    return input % 3 == 0; // placeholder logic
+    let isPrime: boolean = false;
+    return isPrime;
+    //let searchTo: number = 
 }
