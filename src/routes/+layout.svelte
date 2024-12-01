3c6a44dd0b59f00cfbc151ef5aaf8c597bd0756d
acidmath <adrian.ajx@gmail.com>
2024-12-01 08:43:51 -0600

    got dark mode going, can't get a sitewide background though

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import { DarkMode } from 'flowbite-svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
-
+<DarkMode size="lg"/>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,14 @@
 	import { Card } from 'flowbite-svelte';
 	import PrimeBoy from '../components/primeBoy.svelte';
 </script>
-
-<Card>
-	<h1 class="mb-2 dark:text-white">Hello, I am a website created by Adrian Harrison</h1>
-	<h3 class="dark:text-gray-400">He's a desktop developer, so let's not judge me too much</h3>
-</Card>
-<Card>
-	<PrimeBoy />
-</Card>
+<div class="w-full mx-auto flex justify-center">
+	<div class="col-lg-4 col-md-6 col-sm-8 col-11 mx-auto flex-col">
+		<Card>
+			<h1 class="mb-2 dark:text-white">Hello, I am a website created by Adrian Harrison</h1>
+			<h3 class="dark:text-gray-360">He's a C# desktop developer</h3>
+		</Card>
+		<Card>
+			<PrimeBoy />
+		</Card>
+	</div>
+</div>
