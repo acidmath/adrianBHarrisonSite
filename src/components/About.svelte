bdf237bfcaa74ed9b9e5fd59d509ab8c1b30969d
acidmath <adrian.ajx@gmail.com>
2024-12-02 09:07:35 -0600

    update timeline

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -9,7 +9,7 @@
 			Maintain and develop features for .NET 8 WPF applications
 		</p>
 		<p class="text-base font-normal text-gray-700 dark:text-gray-400">
-			Responsibilities - TDD with xUnit | VBA Development | Implement EFCore data access
+			Responsibilities - Expand TDD with xUnit | Port VBA to C# | Implement EFCore data access
 		</p>
 	</TimelineItem>
 	<TimelineItem title="MIMO LLC | Lead Software Developer" date="09/2015 - 11/2023">
@@ -17,7 +17,7 @@
 			Create a Cash Handling software solution from prototype to a product for sale
 		</p>
 		<p class="text-base font-normal text-gray-700 dark:text-gray-400">
-			Responsibilities - Develop Microsoft desktop app stack | Interview and onboard new developers
+			Responsibilities - Develop Microsoft desktop app stack | Interact daily with customers
 			| Collaborate with hardware vendors
 		</p>
 	</TimelineItem>
@@ -26,7 +26,7 @@
 			Internship turned employment, support many projects for various contracts
 		</p>
 		<p class="text-base font-normal text-gray-700 dark:text-gray-400">
-			Responsibilities - Requirements collection | The importance of people skills | How to jump
+			Responsibilities - Collect requirements | Attend networking events to acquire clients | Jump
 			between tech stacks a few times a week
 		</p>
 	</TimelineItem>
