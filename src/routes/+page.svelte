7e5361d6bb1bd9f57293df6ea478db7b87be0440
acidmath <adrian.ajx@gmail.com>
2024-12-02 09:16:58 -0600

    at least fit it all on one page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,19 @@
 	import About from '../components/About.svelte';
 </script>
 
-<div class="min-vh-100">
-	<div class="flex h-full w-full flex-wrap justify-center">
-		<div class="p-10">
-			<About />
-		</div>
+<div class="flex h-full w-full flex-wrap justify-center">
+	<div class="flex col-3 flex-wrap justify-center">
+		<Card class="col-lg-4 col-md-6 col-sm-8 col-11 m-5">
+			<h1 class="mb-2 dark:text-white">Hello, I am a website created by Adrian Harrison</h1>
+			<h3 class="dark:text-gray-360">Adrian is a C# developer</h3>
+		</Card>
+		<Card class="col-lg-4 col-md-6 col-sm-8 col-11 m-5">
+			<PrimeBoy />
+		</Card>
+	</div>
+	<div class="p-10">
+		<About />
 	</div>
 </div>
-<div class="flex w-full flex-wrap justify-center">
-	<Card class="col-lg-4 col-md-6 col-sm-8 col-11 m-5">
-		<h1 class="mb-2 dark:text-white">Hello, I am a website created by Adrian Harrison</h1>
-		<h3 class="dark:text-gray-360">Adrian is a C# developer</h3>
-	</Card>
-	<Card class="col-lg-4 col-md-6 col-sm-8 col-11 m-5">
-		<PrimeBoy />
-	</Card>
-</div>
+
+
