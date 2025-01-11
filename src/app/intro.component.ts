5b90b2431bef454dccaf0aa416b02097d59c5ff5
acidmath <adrian.ajx@gmail.com>
2025-01-11 15:54:13 -0600

    link in game

--- src/app/intro.component.ts
+++ src/app/intro.component.ts
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
     <section class="flex-center">
         <h1 class="m-2">Hello, I am a website created by Adrian Harrison</h1>
         <h3>Adrian is a C# developer</h3>
+        <a href="https://6782e7c1668a59453e0b1c2a--super-panda-456179.netlify.app/">Space Lander</a>
     </section>
     `
 })
